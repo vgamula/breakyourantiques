68cf41782cfbe155981ce37da0b4810584f9425a
Volodymyr Gamula <v.gamula@gmail.com>
2015-04-09 23:07:05 +0300

    Changed fabfile

--- fabfile.py
+++ fabfile.py
@@ -10,6 +10,25 @@ def rsync():
     rsync_project(remote_dir='/var/www/breakyourantiques', exclude=RSYNC_EXCLUDES, delete=True)
 
 
+def start_server():
+    run('sudo restart breakyourantiques')
+
+
+def create_virtualenv():
+    run('virtualenv /var/www/breakyourantiques/venv')
+
+
+def install():
+    rsync()
+    create_virtualenv()
+    install_dependencies()
+    run('cp /var/www/breakyourantiques/breakyourantiques/server-configuration/nginx /etc/nginx/sites-available/breakyourantiques')
+    run('cp /var/www/breakyourantiques/breakyourantiques/server-configuration/upstart /etc/init/breakyourantiques')
+    run('ln -s /etc/nginx/sites-available/breakyourantiques /etc/nginx/sites-enabled')
+    start_server()
+    run('/etc/init.d/nginx restart')
+
+
 def install_dependencies():
     with prefix('source /var/www/breakyourantiques/venv/bin/activate'):
         run('/var/www/breakyourantiques/venv/bin/pip install -r /var/www/breakyourantiques/breakyourantiques/requirements.txt -U')
@@ -18,4 +37,4 @@ def install_dependencies():
 def deploy():
     rsync()
     install_dependencies()
-    run('sudo restart breakyourantiques')
+    start_server()
