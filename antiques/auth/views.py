599e7dd502e486d6a4f32c2e74f98f038ff7d448
Volodymyr Gamula <v.gamula@gmail.com>
2015-04-19 00:44:21 +0300

    Added register view

--- antiques/auth/views.py
+++ antiques/auth/views.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template
+import urllib
+from flask import g, Blueprint, render_template, request, redirect
+from flask.ext.login import login_user, logout_user
 
 from antiques import route, login_manager
-from antiques.core.decorators import logout_required
+from antiques.core.decorators import login_required, logout_required
 
+from .forms import RegisterForm
 from .models import User
 
 
@@ -18,3 +21,27 @@ def load_user(user_id):
 @logout_required
 def login():
     return render_template('auth/login.html')
+
+
+@route(auth_module, '/register', methods=['GET', 'POST'])
+@logout_required
+def register():
+    next = urllib.quote(request.args.get('next', ''))
+    form = RegisterForm(request.form)
+    if form.validate_on_submit():
+        user = form.save()
+        login_user(user)
+        return redirect('/')
+    return render_template(
+        'user/register.html',
+        title='Sign Up',
+        form=form,
+        next=next
+    )
+
+
+@route(auth_module, '/logout')
+@login_required
+def logout():
+    logout_user(g.user)
+    return redirect('/')
