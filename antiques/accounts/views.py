3177a56dc616e85bd33813d7ed3cbbea1a874710
Volodymyr Gamula <v.gamula@gmail.com>
2015-04-18 23:04:21 +0300

    Configure flask-login

--- antiques/accounts/views.py
+++ antiques/accounts/views.py
@@ -1,9 +1,16 @@
 from flask import Blueprint, render_template
 
-from antiques import route
+from antiques import route, login_manager
+
+from .models import User
 
 
 accounts_module = Blueprint('user', __name__, template_folder='templates/accounts')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.find_one(id=int(user_id))
 
 
 @route(accounts_module, '/login', methods=['GET', 'POST'])
