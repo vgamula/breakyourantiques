d15ce6729a112b493bf690bca16b8c4183d24603
Volodymyr Gamula <v.gamula@gmail.com>
2015-04-20 01:41:47 +0300

    Modified ActiveRecordMixin

--- antiques/core/mixins.py
+++ antiques/core/mixins.py
@@ -2,9 +2,10 @@ from antiques import db
 
 
 class ActiveRecordMixin():
-    def save(self):
+    def save(self, commit=True):
         db.session.add(self)
-        db.session.commit()
+        if commit:
+            db.session.commit()
 
     def delete(self):
         db.session.delete(self)
